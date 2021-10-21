b05e20cf3b92b56391f1e663ea1061f1af5b9b4f
tim <luo.tian@live.cn>
2021-10-21 23:46:37 +0800

    0.9.9

--- src-bex/js/background.js
+++ src-bex/js/background.js
@@ -95,12 +95,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 
 
-chrome.browserAction.onClicked.addListener((/* tab */) => {
-  // Opens our extension in a new browser window.
-  // Only if a popup isn't defined in the manifest.
-  chrome.tabs.create({
-    url: chrome.extension.getURL('www/index.html#/')
-  }, (/* newTab */) => {
-    // Tab opened.
-  })
-})
+// chrome.browserAction.onClicked.addListener((/* tab */) => {
+//   // Opens our extension in a new browser window.
+//   // Only if a popup isn't defined in the manifest.
+//   chrome.tabs.create({
+//     url: chrome.extension.getURL('www/index.html#/options')
+//   }, (/* newTab */) => {
+//     // Tab opened.
+//   })
+// })
